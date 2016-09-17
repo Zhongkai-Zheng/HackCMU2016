159efdb303e95eb15f92d19d8492e58053a65a63
Alex Zhongkai Zheng <Alex_Zheng@Zhongkai.wv.cc.cmu.edu>
2016-09-17 05:35:42 -0400

    Happy hacking

---
+++ HackCMU2016/client/addevent.js
@@ -0,0 +1,39 @@
+Template.SellBook.events({
+	'submit form': function(event, template){
+
+		event.preventDefault();
+			
+			var user = Meteor.user();
+			var fileInput = event.target.fileInput;
+			var files = fileInput.files;
+			var title = event.target.title.value;
+			var firstname = event.target.firstname.value;
+			var lastname = event.target.lastname.value;
+			var isbn = event.target.isbn.value; 
+			var reserved = false;
+
+		for (var i = 0, ln = files.length; i < ln; i++) {
+			var newFile = new FS.File(files[i]);
+			newFile.username = Meteor.user().emails[0].address;
+			newFile.userId = user._id;
+			newFile.title = title;
+			newFile.firstname = firstname + " ";
+			newFile.lastname = lastname;
+			newFile.isbn=isbn;
+			newFile.reserved = reserved;
+			newFile.reservedId = null;
+			newFile.createdAt = new Date();
+			}
+
+
+			// TODO: Create GridFS collection and add that here
+
+			Eventslist.insert(newFile, function (err, fileObj) {
+				if (err) {
+					console.log(err);
+				} else {
+					console.log("Success");
+				}
+			});
+		}
+})
